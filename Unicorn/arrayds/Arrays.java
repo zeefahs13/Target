d5a27fcfb3f0694b12a5f6ac5c3e3a6599450e6e
zeefahs13 <32613838+zeefahs13@users.noreply.github.com>
2020-06-19 10:27:15 +0530

    Adding Arrays methos

---
+++ Unicorn/arrayds/Arrays.java
@@ -0,0 +1,40 @@
+package Unicorn.arrayds;
+
+public class Arrays{
+
+
+    /*
+        One cannot compute the sum of elements of an array without visiting them if array elements are 
+        not known in before hand.
+
+        If the array elements are known in before hand, we can compute in O(1) time and O(1) space complexity
+    */
+
+
+    //O(n) solution
+private void sumOfElements(){
+
+
+
+
+
+
+}
+
+
+
+public static class ArraysTest{
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
